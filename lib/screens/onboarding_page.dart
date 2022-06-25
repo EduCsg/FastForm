4b85640bd2bbadb2d394fc47e5fe3f2ebcc864b7
EduCsg <eduardocasagrande71@gmail.com>
2022-06-25 00:52:21 -0300

    Finaliza primeira tela da OnboardingScreen

--- lib/screens/onboarding_page.dart
+++ lib/screens/onboarding_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:flutter_svg/svg.dart';
 
 import './home_page.dart';
 
@@ -31,10 +32,64 @@ class _OnboardingPageState extends State<OnboardingPage> {
               setState(() => isLastPage = index == 2);
             },
             children: [
+              //pagina 1
               Container(
-                color: Colors.red,
-                child: const Center(
-                  child: Text('Page 1'),
+                color: Colors.white,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset(
+                        "assets/images/img1_onboardingpage.svg",
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      const Text(
+                        "O que é a Fast Form?",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      RichText(
+                        textAlign: TextAlign.center,
+                        text: const TextSpan(
+                          text: "A ",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w200,
+                          ),
+                          children: [
+                            TextSpan(
+                              text: 'Fast Form',
+                              style: TextStyle(
+                                fontStyle: FontStyle.italic,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                                fontSize: 18,
+                              ),
+                            ),
+                            TextSpan(
+                              text:
+                                  " é um aplicativo\nque visa a facilitação de\npreenchimento de\nformularios hospitalares.",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w200,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
               Container(
