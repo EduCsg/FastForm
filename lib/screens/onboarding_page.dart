2e8b316d595a90fc48a2f613988d214ca00f599e
EduCsg <eduardocasagrande71@gmail.com>
2022-06-25 00:54:18 -0300

    Finaliza segunda tela da OnboardingScreen

--- lib/screens/onboarding_page.dart
+++ lib/screens/onboarding_page.dart
@@ -92,10 +92,50 @@ class _OnboardingPageState extends State<OnboardingPage> {
                   ),
                 ),
               ),
+              //pagina 2
               Container(
-                color: Colors.green,
-                child: const Center(
-                  child: Text('Page 2'),
+                color: Colors.white,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset(
+                        "assets/images/img2_onboardingpage.svg",
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const Text(
+                        "Como Funciona?",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      RichText(
+                        textAlign: TextAlign.center,
+                        text: const TextSpan(
+                          style: TextStyle(fontSize: 18, color: Colors.black),
+                          children: [
+                            TextSpan(
+                              text:
+                                  " Após preencher o prontuário,\nna próxima vez que você for\nao hospital, basta aproximar o\naplicativo do leitor e pronto.\nO seu prontuário foi enviado!",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w200,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
               Container(
