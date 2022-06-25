d7d4b91d2e8f981c12906256c53766b009dbeb1b
EduCsg <eduardocasagrande71@gmail.com>
2022-06-25 00:42:54 -0300

    Altera o caminho do import da HomePage

--- lib/screens/onboarding_page.dart
+++ lib/screens/onboarding_page.dart
@@ -1,7 +1,8 @@
-import 'package:fast_form_client/screens/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+import './home_page.dart';
 
 class OnboardingPage extends StatefulWidget {
   const OnboardingPage({Key? key}) : super(key: key);
