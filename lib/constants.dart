10fa714da786e94a074ff173e3e279e009c0d62d
EduCsg <eduardocasagrande71@gmail.com>
2022-07-09 00:43:53 -0300

    Cria as contantes em um arquivo separado

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color iconYellowColor = const Color.fromRGBO(255, 182, 29, 1);
+Color labelTextColor = const Color.fromRGBO(120, 120, 120, 1);
+Color textColor = const Color.fromRGBO(47, 47, 47, 1);
+
+const double kDefaultPadding = 20;
