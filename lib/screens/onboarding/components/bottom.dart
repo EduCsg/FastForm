fa30e26b14161b363259ac09085ccb8a36834662
EduCsg <eduardocasagrande71@gmail.com>
2022-07-15 01:39:33 -0300

    Remove import inutilizado

--- lib/screens/onboarding/components/bottom.dart
+++ lib/screens/onboarding/components/bottom.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-import '../../home/home_page.dart';
-
 class BuildOnboardingBottom extends StatelessWidget {
   const BuildOnboardingBottom({
     Key? key,
