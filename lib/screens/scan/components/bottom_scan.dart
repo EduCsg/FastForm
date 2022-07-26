b158b56abfad84116faf480d4cf6254182a2ec61
EduCsg <eduardocasagrande71@gmail.com>
2022-07-26 08:23:07 -0300

    Cria o Footer e o FloatingActionButton

--- lib/screens/scan/components/bottom_scan.dart
+++ lib/screens/scan/components/bottom_scan.dart
@@ -1,88 +1,105 @@
-import 'package:barcode_widget/barcode_widget.dart';
-import 'package:fast_form_client/screens/home/components/imports.dart';
 import 'package:flutter/material.dart';
 
-import '../../../components/model/json_data.dart';
 import '../../../constants.dart';
 
-SizedBox buildBottomScan(
-  BuildContext context,
-  Size size,
-  TextEditingController nomeInput,
-  TextEditingController maeInput,
-  TextEditingController paiInput,
-  TextEditingController dataInput,
-  TextEditingController telefoneInput,
-  TextEditingController enderecoInput,
-  TextEditingController numInput,
-  TextEditingController cpfInput,
-  TextEditingController rgInput,
-) {
-  return SizedBox(
-    height: kDefaultPadding * 5,
-    child: Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 14),
-      child: Column(
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              showDialog(
-                context: context,
-                builder: (_) => AlertDialog(
-                  title: const Text(
-                    'As informações estão corretas?',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.center,
-                  ),
-                  content: BarcodeWidget(
-                    barcode: Barcode.qrCode(),
-                    data:
-                        'Informações: ${jsonData.toJson()}, medicamentos: ${lists.medicamentoListString.toString()}',
-                    width: 225,
-                    height: 225,
-                  ),
-                  actions: [
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: const Color.fromRGBO(91, 217, 189, 1),
-                      ),
-                      child: const Text(
-                        'Fechar',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                    ),
-                  ],
-                ),
-                barrierDismissible: false,
-              );
-            },
-            style: ElevatedButton.styleFrom(
-              primary: const Color.fromRGBO(91, 217, 189, 1),
-              fixedSize: Size(
-                size.width - 60,
-                35,
-              ),
-            ),
-            child: const Text('Gerar QrCode'),
-          ),
-          const SizedBox(
-            height: kDefaultPadding * 0.25,
-          ),
-          Text(
-            'Você pode alterar as informações a qualquer momento',
-            style: TextStyle(
-              color: textColor,
-              fontWeight: FontWeight.w300,
-              fontSize: 10,
-            ),
-          )
-        ],
+class BuildBottomScan extends StatelessWidget {
+  const BuildBottomScan({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: Text(
+        'As informações escaneadas podem ser editadas apenas\npela via do cliente.',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: textColor,
+          fontWeight: FontWeight.w300,
+          fontSize: 10,
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
+
+// SizedBox buildBottomScan(
+//   BuildContext context,
+//   Size size,
+//   TextEditingController nomeInput,
+//   TextEditingController maeInput,
+//   TextEditingController paiInput,
+//   TextEditingController dataInput,
+//   TextEditingController telefoneInput,
+//   TextEditingController enderecoInput,
+//   TextEditingController numInput,
+//   TextEditingController cpfInput,
+//   TextEditingController rgInput,
+// ) {
+//   return SizedBox(
+//     height: kDefaultPadding * 5,
+//     child: Padding(
+//       padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 14),
+//       child: Column(
+//         children: [
+//           ElevatedButton(
+//             onPressed: () {
+//               showDialog(
+//                 context: context,
+//                 builder: (_) => AlertDialog(
+//                   title: const Text(
+//                     'As informações estão corretas?',
+//                     style: TextStyle(fontWeight: FontWeight.bold),
+//                     textAlign: TextAlign.center,
+//                   ),
+//                   content: BarcodeWidget(
+//                     barcode: Barcode.qrCode(),
+//                     data:
+//                         'Informações: ${jsonData.toJson()}, medicamentos: ${lists.medicamentoListString.toString()}',
+//                     width: 225,
+//                     height: 225,
+//                   ),
+//                   actions: [
+//                     ElevatedButton(
+//                       style: ElevatedButton.styleFrom(
+//                         primary: const Color.fromRGBO(91, 217, 189, 1),
+//                       ),
+//                       child: const Text(
+//                         'Fechar',
+//                         style: TextStyle(
+//                           color: Colors.white,
+//                         ),
+//                       ),
+//                       onPressed: () {
+//                         Navigator.of(context).pop();
+//                       },
+//                     ),
+//                   ],
+//                 ),
+//                 barrierDismissible: false,
+//               );
+//             },
+//             style: ElevatedButton.styleFrom(
+//               primary: const Color.fromRGBO(91, 217, 189, 1),
+//               fixedSize: Size(
+//                 size.width - 60,
+//                 35,
+//               ),
+//             ),
+//             child: const Text('Gerar QrCode'),
+//           ),
+//           const SizedBox(
+//             height: kDefaultPadding * 0.25,
+//           ),
+//           Text(
+//             'Você pode alterar as informações a qualquer momento',
+//             style: TextStyle(
+//               color: textColor,
+//               fontWeight: FontWeight.w300,
+//               fontSize: 10,
+//             ),
+//           )
+//         ],
+//       ),
+//     ),
+//   );
+// }
--- lib/screens/scan/scan_page.dart
+++ lib/screens/scan/scan_page.dart
@@ -1,3 +1,4 @@
+import 'package:fast_form_client/constants.dart';
 import 'package:flutter/material.dart';
 
 import 'components/body_scan.dart';
@@ -16,24 +17,17 @@ final inputsContollers = InputsControllers();
 class _ScanPageState extends State<ScanPage> {
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-
     return Scaffold(
       appBar: buildAppBarScan(),
       body: const BuildBodyScan(),
-      bottomNavigationBar: buildBottomScan(
-        context,
-        size,
-        inputsContollers.nomeInput,
-        inputsContollers.maeInput,
-        inputsContollers.paiInput,
-        inputsContollers.dataInput,
-        inputsContollers.telefoneInput,
-        inputsContollers.enderecoInput,
-        inputsContollers.numInput,
-        inputsContollers.cpfInput,
-        inputsContollers.rgInput,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: scannerColor,
+        child: const Icon(
+          Icons.qr_code,
+        ),
       ),
+      bottomNavigationBar: const BuildBottomScan(),
     );
   }
 }
