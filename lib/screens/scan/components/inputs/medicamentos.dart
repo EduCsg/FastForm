9f73b2289f80d4cdd306d1ab03e0dfccbb13cbc8
EduCsg <eduardocasagrande71@gmail.com>
2022-07-29 08:49:21 -0300

    Atualiza o valor dos inputs ao escanear

--- lib/screens/scan/components/inputs/medicamentos.dart
+++ lib/screens/scan/components/inputs/medicamentos.dart
@@ -12,33 +12,10 @@ class WidgetMedicamentoInput extends StatefulWidget {
   State<WidgetMedicamentoInput> createState() => _WidgetMedicamentoInputState();
 }
 
-final lists = MedicamentoListString();
+final listsScan = MedicamentoListScan();
 
 class _WidgetMedicamentoInputState extends State<WidgetMedicamentoInput> {
   TextEditingController medicamentoInput = TextEditingController();
-  final List<_Medicamento> _medicamentoListWidget = [];
-
-  void removeServiceCard(index) {
-    setState(() {
-      _medicamentoListWidget.remove(index);
-      lists.medicamentoListString
-          .removeAt(lists.medicamentoListString.indexOf(index.yourText));
-    });
-  }
-
-  void _addMedicamento() {
-    setState(() {
-      lists.medicamentoListString.add(
-        medicamentoInput.text,
-      );
-      _medicamentoListWidget.add(
-        _Medicamento(
-          onDelete: removeServiceCard,
-          yourText: medicamentoInput.text,
-        ),
-      );
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -68,19 +45,6 @@ class _WidgetMedicamentoInputState extends State<WidgetMedicamentoInput> {
           elevation: 1,
           child: TextField(
             textInputAction: TextInputAction.send,
-            onSubmitted: (value) {
-              if (medicamentoInput.text.isNotEmpty) {
-                if (lists.medicamentoListString
-                    .contains(medicamentoInput.text)) {
-                  Fluttertoast.showToast(msg: "Medicamento ja registrado!");
-                  medicamentoInput.clear();
-                  return;
-                } else {
-                  _addMedicamento();
-                  medicamentoInput.clear();
-                }
-              }
-            },
             controller: medicamentoInput,
             decoration: InputDecoration(
               border: InputBorder.none,
@@ -93,29 +57,6 @@ class _WidgetMedicamentoInputState extends State<WidgetMedicamentoInput> {
                 color: labelTextColor,
                 fontSize: 16,
               ),
-              suffixIcon: IconButton(
-                splashColor: Colors.transparent,
-                highlightColor: Colors.transparent,
-                onPressed: () {
-                  if (medicamentoInput.text.isNotEmpty) {
-                    if (lists.medicamentoListString
-                        .contains(medicamentoInput.text)) {
-                      Fluttertoast.showToast(msg: "Medicamento ja registrado!");
-                      medicamentoInput.clear();
-                      FocusScope.of(context).requestFocus(FocusNode());
-                      return;
-                    } else {
-                      _addMedicamento();
-                      medicamentoInput.clear();
-                    }
-                  }
-                  FocusScope.of(context).requestFocus(FocusNode());
-                },
-                icon: Icon(
-                  Icons.add,
-                  color: scannerColor,
-                ),
-              ),
             ),
           ),
         ),
@@ -125,20 +66,18 @@ class _WidgetMedicamentoInputState extends State<WidgetMedicamentoInput> {
         Wrap(
           spacing: 8.0,
           runSpacing: 4.0,
-          children: _medicamentoListWidget,
+          children: listsScan.medicamentoListWidget,
         ),
       ],
     );
   }
 }
 
-class _Medicamento extends StatelessWidget {
-  final Function(_Medicamento) onDelete;
+class Medicamento extends StatelessWidget {
   final String yourText;
 
-  const _Medicamento({
+  const Medicamento({
     Key? key,
-    required this.onDelete,
     required this.yourText,
   }) : super(key: key);
 
@@ -165,15 +104,10 @@ class _Medicamento extends StatelessWidget {
                     ),
                   ),
                   const SizedBox(width: 12),
-                  IconButton(
-                    icon: const Icon(Icons.close),
-                    color: const Color.fromRGBO(15, 15, 15, 1),
-                    iconSize: 18,
-                    padding: EdgeInsets.zero,
-                    constraints: const BoxConstraints(),
-                    splashColor: Colors.transparent,
-                    highlightColor: Colors.transparent,
-                    onPressed: () => onDelete(this),
+                  const Icon(
+                    Icons.close,
+                    color: Color.fromRGBO(15, 15, 15, 1),
+                    size: 18,
                   ),
                 ],
               ),
@@ -185,6 +119,6 @@ class _Medicamento extends StatelessWidget {
   }
 }
 
-class MedicamentoListString {
-  final List<String> medicamentoListString = [];
+class MedicamentoListScan {
+  List<Medicamento> medicamentoListWidget = [];
 }
--- lib/screens/scan/components/scanner_button.dart
+++ lib/screens/scan/components/scanner_button.dart
@@ -1,8 +1,12 @@
+import 'package:fast_form_client/screens/home/home_page.dart';
 import 'package:fast_form_client/screens/scan/components/body_scan.dart';
+import 'package:fast_form_client/screens/scan/scan_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 import '../../../constants.dart';
+import 'imports.dart';
 
 class BuildScannerButton extends StatefulWidget {
   const BuildScannerButton({Key? key}) : super(key: key);
@@ -21,18 +25,44 @@ class BuildScannerButtonState extends State<BuildScannerButton> {
       false,
       ScanMode.QR,
     );
-    setState(() => resEscaneadoBruto = code != '-1' ? code : 'Não Validado');
 
-    var arr = resEscaneadoBruto.split('*');
-    inputsContollersScan.nomeInput.text = arr[0];
-    inputsContollersScan.maeInput.text = arr[1];
-    inputsContollersScan.paiInput.text = arr[2];
-    inputsContollersScan.dataInput.text = arr[3];
-    inputsContollersScan.telefoneInput.text = arr[4];
-    inputsContollersScan.enderecoInput.text = arr[5];
-    inputsContollersScan.numInput.text = arr[6];
-    inputsContollersScan.cpfInput.text = arr[7];
-    inputsContollersScan.rgInput.text = arr[8];
+    void updateData() {
+      setState(() => resEscaneadoBruto = code);
+
+      var resDividido = resEscaneadoBruto.split('*');
+
+      listsScan.medicamentoListWidget.add(
+        Medicamento(
+          yourText: resDividido.last,
+        ),
+      );
+
+      var i = 0;
+      inputsContollersScan.nomeInput.text = resDividido[i++];
+      inputsContollersScan.maeInput.text = resDividido[i++];
+      inputsContollersScan.paiInput.text = resDividido[i++];
+      inputsContollersScan.dataInput.text = resDividido[i++];
+      inputsContollersScan.telefoneInput.text = resDividido[i++];
+      inputsContollersScan.enderecoInput.text = resDividido[i++];
+      inputsContollersScan.numInput.text = resDividido[i++];
+      inputsContollersScan.cpfInput.text = resDividido[i++];
+      inputsContollersScan.rgInput.text = resDividido[i++];
+    }
+
+    if (code == '-1') {
+      Fluttertoast.showToast(msg: "Código não válido!");
+      return;
+    } else {
+      updateData();
+    }
+
+    if (!mounted) return;
+    Navigator.of(context).pop();
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const ScanPage(),
+      ),
+    );
   }
 
   @override
