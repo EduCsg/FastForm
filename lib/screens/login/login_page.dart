8bcaa56905eed247b17c68364eb2dbdd1ad56e9c
EduCsg <eduardocasagrande71@gmail.com>
2022-08-13 13:23:18 -0300

    Adiciona opção de ir para a OnboardingPage na Login

--- lib/screens/login/login_page.dart
+++ lib/screens/login/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:fast_form/screens/onboarding/onboarding_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -12,18 +13,29 @@ class LoginPage extends StatelessWidget {
     double maxHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(0),
-        child: AppBar(
-          systemOverlayStyle: SystemUiOverlayStyle.dark,
-          backgroundColor: Colors.transparent,
-          elevation: 0,
+      appBar:
+          // preferredSize: const Size.fromHeight(0),
+          AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_new_rounded),
+          onPressed: () => Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const OnboardingPage(),
+            ),
+          ),
+          color: Colors.black,
         ),
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
       ),
-      body: Center(
-        child: SingleChildScrollView(
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               const Text(
                 'Escolha',
